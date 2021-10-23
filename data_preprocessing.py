2e23d10ffa405bc4a4fa5cff0245c32ff440e558
shashank1989 <shashank25.it@gmail.com>
2021-10-23 12:51:58 +0530

    Preprocessing changes

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,1 +1,11 @@
 print("adding all the data processing code related to fraud detection")
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+def sum(a,b):
+    return a+b
+
+print(sum(3,4))
