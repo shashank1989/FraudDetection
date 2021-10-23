047c95febe011158efefa75f4b40d8312b2e6404
shashank1989 <shashank25.it@gmail.com>
2021-10-23 16:15:48 +0530

    Added changes to ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,14 @@
 print("Adding code to new branch mymodel.py")
+
+def model1():
+    print("Adding code to model1")
+
+def model2():
+    print("Adding code to model2")
+
+def model3():
+    print("Adding code to model3")
+
+model1()
+model2()
+model3()
